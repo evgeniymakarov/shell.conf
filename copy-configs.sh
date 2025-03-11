f5c8fa698eca26239dd0f2e120d3eedde0cc55a1
evgenioni <mail@evgenioni.ru>
2025-03-11 03:12:38 +0300

    copy configs hook

--- copy-configs.sh
+++ copy-configs.sh
@@ -1,3 +1,6 @@
 #!/bin/bash 
-cp ~/.vimrc `pwd`/.vimrc
-cp ~/.zshrc `pwd`/.zshrc
+DATASET_REPO_PATH="$(pwd)"
+cp $DATASET_REPO_PATH/../.vimrc $DATASET_REPO_PATH/.vimrc
+cp $DATASET_REPO_PATH/../.zshrc $DATASET_REPO_PATH/.zshrc
+git add $file
+
