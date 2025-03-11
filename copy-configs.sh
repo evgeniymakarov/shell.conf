120da4e1aab9e495c188ea5812309f49b82d7585
evgenioni <mail@evgenioni.ru>
2025-03-11 03:37:12 +0300

    test

--- copy-configs.sh
+++ copy-configs.sh
@@ -2,5 +2,6 @@
 DATASET_REPO_PATH="$(pwd)"
 cp $DATASET_REPO_PATH/../.vimrc $DATASET_REPO_PATH/.vimrc
 cp $DATASET_REPO_PATH/../.zshrc $DATASET_REPO_PATH/.zshrc
-git add $file
+touch .commit
+exit
 
